24199daaeb63defe0339cf0d73832fd443479551
Fei Chen <sdcf2010@gmail.com>
2016-03-08 22:45:28 +0000

    Shell Sort

---
+++ Sort/ShellSort.java
@@ -0,0 +1,37 @@
+public class ShellSort {
+
+    public static void sort(int[] arr) {
+        Utils.print(arr, " ");
+        
+        int d = arr.length/2;
+        while(d >= 1) {
+            shellInsert(arr, d);
+            d /= 2;
+        }
+
+        Utils.result(arr, " ");
+    }
+
+    public static void shellInsert(int[] arr, int d) {
+        System.out.println("=============");
+        System.out.println("====== d=" + d);
+        for(int i = d; i < arr.length; i++) {
+            System.out.println("====== i=" + i);
+            int j = i - d;
+            int temp = arr[i];
+            while(j >= 0 && arr[j] > temp) {
+                arr[j+d] = arr[j];
+                j -= d;
+            }
+            if(j != i-d) {
+                arr[j+d] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {49, 38, 65, 97, 76, 13, 27, 49, 55, 4};
+        sort(arr);
+    }
+
+}
